fb0a3a18addfaeebede33aa5c0bcd1deb46389ec
Itxa64 <71594870+Itxa64@users.noreply.github.com>
2025-03-31 16:03:32 +0200

    Update string_tests.py

--- string_tests.py
+++ string_tests.py
@@ -1,8 +1,12 @@
 import unittest
+from code import ajouter
 
+class TestAjouter(unittest.TestCase):
+    def test_ajouter(self):
+        self.assertEqual(ajouter(2, 3), 5)
+        self.assertEqual(ajouter(-1, 1), 0)
+        self.assertEqual(ajouter(0, 0), 0)
 
-class StringTests(unittest.TestCase):
-    def test_to_upper(self):
-        import uppercase
-        self.assertEqual(uppercase.to_upper('foo'), 'FOO', "Wrong uppercase value for foo")
-        self.assertEqual(uppercase.to_upper('Bar'), 'BAR')
+if __name__ == '__main__':
+    unittest.main()
+
