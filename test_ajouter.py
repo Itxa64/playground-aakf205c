c6b005ce240ffa96d7040a1b3e8211e2952e00b2
Itxa64 <71594870+Itxa64@users.noreply.github.com>
2025-04-18 09:46:05 +0200

    Update test_ajouter.py

--- test_ajouter.py
+++ test_ajouter.py
@@ -2,10 +2,11 @@ import unittest
 from code import ajouter
 
 class TestAjouter(unittest.TestCase):
-    def test_ajouter(self):
+    def test_addition(self):
         self.assertEqual(ajouter(2, 3), 5)
         self.assertEqual(ajouter(-1, 1), 0)
         self.assertEqual(ajouter(0, 0), 0)
+        self.assertEqual(ajouter(100, 250), 350)
 
 if __name__ == "__main__":
     unittest.main()
